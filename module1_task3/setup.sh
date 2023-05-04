12b2645485dfc40eced9c512d1309da8ec062bad
RedginaldGodeau <redginaldgodeau95@gmail.com>
2023-05-04 20:22:40 +0000

    adding executable and update and wget

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get install make -yq && apt-get install hugo -yq \
+apt-get update \
+&& apt-get install make -yq && apt-get install hugo -yq && apt-get install wget -yq \
 && wget https://github.com/gohugoio/hugo/releases/download/v0.84.1/hugo_extended_0.84.1_Linux-64bit.tar.gz \
 && tar -xzvf hugo_extended_0.84.1_Linux-64bit.tar.gz \
 && mv hugo /usr/local/bin/ \
