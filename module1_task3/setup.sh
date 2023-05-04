0188593384f51f89237fc6f4747b826ed6266b62
RedginaldGodeau <redginaldgodeau95@gmail.com>
2023-05-04 20:36:38 +0000

    silente aptget

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt-get update \
-&& apt-get install make -yq && apt-get install hugo -yq && apt-get install wget -yq \
-&& wget https://github.com/gohugoio/hugo/releases/download/v0.84.1/hugo_extended_0.84.1_Linux-64bit.tar.gz \
+apt-get update -qq\
+&& apt-get install make -yqq && apt-get install hugo -yqq && apt-get install wget -yqq \
+&& wget -qq https://github.com/gohugoio/hugo/releases/download/v0.84.1/hugo_extended_0.84.1_Linux-64bit.tar.gz \
 && tar -xzvf hugo_extended_0.84.1_Linux-64bit.tar.gz \
 && mv hugo /usr/local/bin/ \
-&& rm hugo_extended_0.84.1_Linux-64bit.tar.gz && make build
+&& rm hugo_extended_0.84.1_Linux-64bit.tar.gz && make build -s 
