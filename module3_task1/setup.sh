55b89ea30577ec620d48e1b794240cf1384eecd7
RedginaldGodeau <redginaldgodeau95@gmail.com>
2023-05-18 09:58:41 +0000

    update

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 apt-get update && sudo apt-get install -y hugo make git wget python golang
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
 pip install Online-W3C-Validator
