c793eada2bb345f02c52139da5c780feef44af22
RedginaldGodeau <redginaldgodeau95@gmail.com>
2023-05-18 09:52:59 +0000

    update

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+apt-get update && apt-get install -y hugo make git wget python
+wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Linux-64bit.tar.gz
+tar -xf hugo_0.111.3_Linux-64bit.tar.gz
+mv ./hugo /usr/bin/
+wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+pip install Online-W3C-Validator
